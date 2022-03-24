098afefb210ea2efb35751d5ce07f56f3283087e
programas de férias <90060154+AlcionePereira@users.noreply.github.com>
2022-03-23 22:40:11 -0300

    Update listainversa.py

--- listainversa.py
+++ listainversa.py
@@ -1,16 +1,17 @@
 '''9) Dada uma lista X numérica contendo 5 elementos, fazer um programa que crie e exiba na tela
 uma lista Y. A lista Y deverá conter o mesmo conteúdo da lista X na ordem inversa.'''
-from random import randint
-
-max_lista = 5
-listax = list(range(max_lista))
-listay = list()
 
 
-#lista y recebe lista x inversa
-for i in range(max_lista):
-    listax[i] = randint(0, 100)
-    listay = listax[::-1]
-print(listax)
-print(listay)
+def reverseList (lista_y):
+    lista_y.append(lista_x.reverse())
+    return lista_x
+  
+    
+    
 
+lista_y = list()
+lista_x=[1, 2, 3, 4, 5]
+print(lista_x)
+
+print(reverseList (lista_y))
+  
